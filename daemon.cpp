59b1e6b1d6d53b9f86333b5e097a942e7a1a5a5d
Marius Roets <marius.roets@eskom.co.za>
2010-08-18 14:51:50 +0200

    Initial commit

---
+++ daemon.cpp
@@ -0,0 +1,147 @@
+
+
+#include "daemon.h"
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <stdexcept>
+
+Daemon *Daemon::mDaemon = 0;
+
+Daemon::Daemon(const std::string& key) : mKey(key)
+{
+   mStdInputFile = "/dev/null";
+   mStdOutputFile = "/dev/null";
+   mErrOutputFile = "/dev/null";
+   mRunningDirectory = "/";
+   mPid = 0;
+   //qDebug() << "Daemon: Contructing Daemon object";
+   mStatus = lock();
+   switch (mStatus) {
+       case Daemon::New:
+           break;
+       case Daemon::Error:
+           throw std::runtime_error("Could not create a lock file");
+           break;
+       case Daemon::Running:
+           mPid = getPid();
+           break;
+   }
+}
+Daemon::~Daemon()
+{
+   //qDebug() << "Daemon: Destructor";
+   //mSharedMem->detach();
+   //delete mSharedMem;
+}
+Daemon* Daemon::daemon(const std::string& key)
+{
+   if (!mDaemon) {
+      mDaemon = new Daemon(key);
+   }
+   return mDaemon;
+}
+Daemon::Status Daemon::status()
+{
+   return mStatus;
+}
+pid_t Daemon::getPid()
+{
+    return mPid;
+}
+void Daemon::start()
+{
+    if (mStatus != Daemon::New) {
+        //qDebug() << "Daemon: The process is already running";
+        return;
+    }
+    // Fork the daemon
+    pid_t pid = fork();
+    if (pid) 
+        exit(0);
+    pid = fork();
+    if (pid) 
+        exit(0);
+    // Get the process id
+    mPid = getpid();
+    // Create a new process group
+    setsid();
+    // Set file creation permission
+    umask(027);
+    // Close all file descriptors
+    for (int i = getdtablesize(); i >= 0; --i) 
+        close(i);
+    // Redirect standard I/O
+    int fd0 = open(mStdInputFile.c_str(), O_RDONLY);
+    int fd1 = open(mStdOutputFile.c_str(), O_WRONLY|O_CREAT|O_APPEND, S_IRUSR|S_IWUSR);
+    int fd2 = open(mErrOutputFile.c_str(), O_WRONLY|O_CREAT|O_APPEND, S_IRUSR|S_IWUSR);
+    if (fd0 != STDIN_FILENO || fd1 != STDOUT_FILENO || fd2 != STDERR_FILENO) {
+        //Unexpected file descriptors
+        throw std::runtime_error("new standard file descriptors were not opened as expected");
+    }
+
+    // Set the running directory
+    if (!mRunningDirectory.empty() && chdir(mRunningDirectory.c_str()) < 0) {
+        // Error
+        throw std::runtime_error(strerror(errno));
+    }
+
+    // Set up signal handler
+    signal(SIGHUP, Daemon::signalHandler); // hangup signal
+    signal(SIGTERM, Daemon::signalHandler); // termination(kill) signal
+    signal(SIGCHLD,SIG_IGN); // ignore child
+    signal(SIGTSTP,SIG_IGN); // ignore tty signals
+    signal(SIGTTOU,SIG_IGN);
+    signal(SIGTTIN,SIG_IGN);
+
+    // Logging
+
+    mStatus = Daemon::Running;
+
+}
+Daemon::Status Daemon::lock()
+{
+    int lfp = open("/tmp/exampled.lock", O_RDWR|O_CREAT,0640);
+    if (lfp < 0) 
+        return Daemon::Error; // can't open
+    if (lockf(lfp,F_TLOCK, 0) < 0) 
+        return Daemon::Running; // can't lock
+    char str[10];
+    sprintf(str,"%d\n", mPid);
+    write(lfp, str, strlen(str)); // record pid to lockfile
+    return Daemon::New;
+}
+void Daemon::stop()
+{
+    if (mStatus != Daemon::Running) {
+        //qDebug() << "Daemon: The process is not running";
+        return;
+    }
+    //qDebug() << "Daemon: Stopping daemon with PID : " << mPid;
+    exit(0);
+}
+void Daemon::setFiles(const std::string& inputFile, const std::string& outputFile, const std::string& errorFile)
+{
+    mStdInputFile = inputFile;
+    mStdOutputFile = outputFile;
+    mErrOutputFile = errorFile;
+}
+pid_t Daemon::pid()
+{
+    return mPid;
+}
+void Daemon::signalHandler(int signal)
+{
+    switch (signal) {
+        case SIGTERM:
+            if (mDaemon) {
+                //qDebug() << "Daemon: Deleting daemon singleton";
+                delete mDaemon;
+                exit(0);
+            }
+        case SIGHUP:
+            break;
+    }
+}
+
---
+++ daemon.h
@@ -0,0 +1,54 @@
+/***************************************************************************
+                          filename - description
+                             -------------------
+    begin                : 
+    copyright            : (C) yyyy [name] <[email]>
+ ***************************************************************************/
+
+#ifndef __DAEMON_H__
+#define __DAEMON_H__
+
+#include <string>
+#define SHARED_MEM_SIZE 15
+
+class Daemon
+{
+   public:
+      enum Status { 
+         New,
+         Running,
+         Error
+      };
+      ~Daemon();
+      void start();
+      void setFiles(const std::string& inputFile,
+                    const std::string& outputFile,
+                    const std::string& errorFile);
+      pid_t pid();
+      void stop();
+      Status status();
+      static void signalHandler(int signal);
+      static Daemon* daemon(const std::string& key);
+    
+   private:
+      Daemon(const std::string& key);
+      pid_t mPid;
+      std::string mKey;
+      std::string mStdOutputFile;
+      std::string mStdInputFile;
+      std::string mErrOutputFile;
+      std::string mRunningDirectory;
+      std::string mLockFile;
+      Status mStatus;
+
+      static Daemon *mDaemon;
+
+      Status lock();
+      pid_t getPid();
+
+
+
+};
+
+#endif	// __DAEMON_H__
+
