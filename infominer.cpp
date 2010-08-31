0639ca4ad81dea5b0e55544ef40d5e88ec8326cd
Marius Roets <marius.roets@eskom.co.za>
2010-08-31 13:50:01 +0200

    Network and software output

--- infominer.cpp
+++ infominer.cpp
@@ -7,6 +7,8 @@
 #include "memory.h"
 #include "harddrive.h"
 #include "system.h"
+#include "network.h"
+#include "software.h"
 
 
 InfoMiner::InfoMiner()
@@ -16,11 +18,15 @@ InfoMiner::InfoMiner()
     Memory mem;
     HardDrive hd;
     System sys;
+    Software sw;
+    Network nw;
     std::cout << c << std::endl;
     std::cout << os << std::endl;
     std::cout << mem << std::endl;
     std::cout << hd << std::endl;
     std::cout << sys << std::endl;
+    std::cout << nw << std::endl;
+    std::cout << sw << std::endl;
 }
 InfoMiner::~InfoMiner()
 {
