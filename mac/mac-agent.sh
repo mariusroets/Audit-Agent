aa167cf8a5e251727fad96204f4436da90e0542f
admin <admin@admins-imac-g5.local>
2011-08-29 21:10:09 +0200

    Mac deploy files

---
+++ mac/mac-agent.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/sw/sbin/agent -u mroets -a 192.168.2.6 -p anduril1 -f /tmp/audit.txt -d start -s 60
---
+++ mac/mac_install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+install -o root agent /sw/sbin
+install -o root mac-agent.sh /sw/sbin
