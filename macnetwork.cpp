3a35d14c577bdbc81260ea7727021929c4db6fe0
Marius Roets <mroets@marius-laptop.eskom.co.za>
2011-09-14 16:03:39 +0200

    Mac network information retriever

--- macnetwork.cpp
+++ macnetwork.cpp
@@ -1,4 +1,6 @@
 #include "macnetwork.h"
+#include "sysprofileparser.h"
+
 MacNetwork::MacNetwork()
 {
     read();
@@ -8,5 +10,42 @@ MacNetwork::~MacNetwork()
 }
 void MacNetwork::read()
 {
+    mAdapters.clear();
+    SysProfileParser sys;
+    sys.parse();
+
+    // Adapter 1 : Modem
+    string value = sys.value("Network:Internal Modem:Type");
+    if (!value.empty()) {
+        mAdapters.push_back(Adapter());
+        int index = mAdapters.size() - 1;
+        mAdapters[index].name = sys.value("Network:Internal Modem:BSD Device Name");
+        mAdapters[index].mac = "";
+        mAdapters[index].ip = sys.value("Network:Internal Modem:IPv4:Addresses");
+        mAdapters[index].mask = sys.value("Network:Internal Modem:IPv4:Subnet Masks");
+        mAdapters[index].count = 1;
+    }
+    // Adapter 2 : Ethernet
+    value = sys.value("Network:Ethernet:Type");
+    if (!value.empty()) {
+        mAdapters.push_back(Adapter());
+        int index = mAdapters.size() - 1;
+        mAdapters[index].name = sys.value("Network:Ethernet:BSD Device Name");
+        mAdapters[index].mac = sys.value("Network:Ethernet:Ethernet:MAC Address");
+        mAdapters[index].ip = sys.value("Network:Ethernet:IPv4:Addresses");
+        mAdapters[index].mask = sys.value("Network:Ethernet:IPv4:Subnet Masks");
+        mAdapters[index].count = 1;
+    }
+    // Adapter 3 : Firewire
+    value = sys.value("Network:FireWire:Type");
+    if (!value.empty()) {
+        mAdapters.push_back(Adapter());
+        int index = mAdapters.size() - 1;
+        mAdapters[index].name = sys.value("Network:FireWire:BSD Device Name");
+        mAdapters[index].mac = sys.value("Network:FireWire:Ethernet:MAC Address");
+        mAdapters[index].ip = sys.value("Network:FireWire:IPv4:Addresses");
+        mAdapters[index].mask = sys.value("Network:FireWire:IPv4:Subnet Masks");
+        mAdapters[index].count = 1;
+    }
 }
 
