406451fd170aa954c543eb65ae57c389cc53edd1
Marius Roets <mroets@marius-laptop.eskom.co.za>
2011-09-14 15:23:20 +0200

    Use factory to instantiate objects

--- infominer.cpp
+++ infominer.cpp
@@ -22,13 +22,13 @@ InfoMiner::~InfoMiner()
 std::ostream& operator<<(std::ostream& stream, InfoMiner& im)
 {
     vector<Info*> info;
-    info.push_back(new Asset);
-    info.push_back(new CPU);
-    info.push_back(new OS);
-    info.push_back(new Memory);
-    info.push_back(new HardDrive);
-    info.push_back(new System);
-    info.push_back(new Software);
+    info.push_back(Info::Factory(Info::Asset));
+    info.push_back(Info::Factory(Info::CPU));
+    info.push_back(Info::Factory(Info::OS));
+    info.push_back(Info::Factory(Info::Memory));
+    info.push_back(Info::Factory(Info::HardDrive));
+    info.push_back(Info::Factory(Info::System));
+    info.push_back(Info::Factory(Info::Software));
     info.push_back(Info::Factory(Info::Network));
 
     for (int i = 0; i < (int)info.size(); i++) {
