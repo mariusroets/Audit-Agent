d18559ded2767c9740ab51e6524c2b7f022e75ea
Marius Roets <mroets@marius-laptop.eskom.co.za>
2011-09-14 16:17:34 +0200

    Mac cpu implementation

--- maccpu.cpp
+++ maccpu.cpp
@@ -1,9 +1,7 @@
 
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <boost/algorithm/string.hpp>
+#include <cstdlib>
 #include "maccpu.h"
+#include "sysprofileparser.h"
 
 MacCPU::MacCPU()
 {
@@ -14,34 +12,13 @@ MacCPU::~MacCPU()
 }
 void MacCPU::read()
 {
-    std::string line;
-    std::ifstream file ("/proc/cpuinfo");
-    if (file.is_open()) {
-        while (!file.eof()) {
-            getline (file, line);
-            boost::trim(line);
-            // Skip empty lines
-            if (line.empty())
-                continue;
-            std::vector<std::string> tokens;
-            boost::split(tokens, line, boost::is_any_of(":"));
-            // Skip lines with no information (just one field)
-            if (tokens.size() < 2)
-                continue;
-            // Remove whitespace
-            boost::trim(tokens[0]);
-            boost::trim(tokens[1]);
-            // Populate data members
-            if (tokens[0] == "vendor_id") {
-                mVendor = tokens[1];
-            } else if (tokens[0] == "cpu cores") {
-                mCount = atoi(tokens[1].c_str());
-            } else if (tokens[0] == "cpu MHz") {
-                mSpeed = atoi(tokens[1].c_str());
-            } else if (tokens[0] == "CPU Reg String") {
-                mRegString = tokens[1];
-            }
+    SysProfileParser sys;
+    sys.parse();
 
-        }
-    }
+    mCount = atoi(sys.value("Hardware:Hardware Overview:Number Of CPUs").c_str());
+    mSpeed = sys.value("Hardware:Hardware Overview:Processor Speed");
+    mType = sys.value("Hardware:Hardware Overview:Processor Name");
+    mIdString = sys.value("Hardware:Hardware Overview:Model Identifier");
+    mSerial = sys.value("Hardware:Hardware Overview:Serial Number (system)");
+
 }
