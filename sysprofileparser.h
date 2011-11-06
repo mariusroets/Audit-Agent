5b48a038200223b9406ca5d95c771aa538e46cbf
Marius Roets <roets.marius@gmail.com>
2011-11-06 15:12:38 +0200

    Reverted to earlier version

--- sysprofileparser.h
+++ sysprofileparser.h
@@ -10,58 +10,30 @@
 
 #include <string>
 #include <vector>
-#include <map>
-#include "tinyxml.h"
-#include "harddrive.h"
 
 using namespace std;
 
-/* class ArrayItem;
-typedef vector<ArrayItem> Array;
-class DictItem {
-    public:
-        enum DictItemType { String, Dict, Array, Integer };
-        DictItem();
-
-        DictItemType type;
-        string str_val;
-        int int_val;
-        DictItem *dict_val;
-        Array arr_val;
+struct Node {
+    Node() {
+        name = "";
+        value = "";
+    }
+    string name;
+    string value;
 };
-typedef map<string, DictItem> Dictionary;
-class ArrayItem {
-    public:
-        enum ArrayItemType { String, Dict };
-        ArrayItem();
-
-        string item;
-        Dictionary dict;
-}; */
-
 
 class SysProfileParser
 {
     public:
-        enum PredefinedValue { IpAddress, MacAddress };
         SysProfileParser();
         ~SysProfileParser();
         void parse();
         string value(const vector<string>& key_list);
-        string value(const string& key_list);
-        string value(PredefinedValue pre);
-        void traverse(TiXmlNode* node);
-        string stackList();
 
     private:
-        // Disk drive information
-        std::map<std::string, HardDrive::DiskDevice> mDevices;
-        void setCurrentDataType(TiXmlNode* node);
+        vector<Node> mNodes;
 
-        //Array data;
-        string current_element;
-        string current_data_type;
-        vector<string> element_stack;
+        bool isValueNode(Node n);
 };
 
 #endif	// __SYSPROFILEPARSER_H__
