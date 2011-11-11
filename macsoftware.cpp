64037e1ce4e076c03d2b3d4e93021200a1e0fcb8
Marius Roets <roets.marius@gmail.com>
2011-11-11 11:01:03 +0200

    Mac software scanner

--- macsoftware.cpp
+++ macsoftware.cpp
@@ -1,5 +1,6 @@
 
 #include "macsoftware.h"
+#include "commandparser.h"
 
 MacSoftware::MacSoftware()
 {
@@ -10,4 +11,22 @@ MacSoftware::~MacSoftware()
 }
 void MacSoftware::read()
 {
+    CommandParser c;
+    vector<string> lines = c.parse("cat pkgutil.txt");
+    for (int i = 0; i < (int)lines.size(); i++) {
+        mSoftwareList.push_back(SoftwarePackage());
+        mSoftwareList[i].name = lines[i];
+        CommandParser c2;
+        vector<string> lines2 = c2.parse(string("pkgutil --pkg-info ") + lines[i]);
+        vector<vector<string> > fields = c2.split(":");
+        for (int j = 0; j < (int)lines2.size(); j++) {
+            if (fields[j][0] == "version") {
+                mSoftwareList[i].version = fields[j][1];
+            }
+            if (fields[j][0] == "install-time") {
+                mSoftwareList[i].install_time = fields[j][1];
+            }
+            
+        }
+    }
 }
