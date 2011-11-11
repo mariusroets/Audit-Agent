f13e758ad52e5b8712e5f6a96c9c0884f57b4f4d
Marius Roets <roets.marius@gmail.com>
2011-11-11 11:00:13 +0200

    Software data structure and output

--- software.cpp
+++ software.cpp
@@ -11,10 +11,20 @@ Software::~Software()
 
 std::string Software::output()
 {
+
     stringstream stream;
     stream << "Hotfix Count=" << 0 << endl;
     stream << endl;
-    stream << "Software Count=" << 0 << endl;
+    stream << "Software Count=" << mSoftwareList.size() << endl;
+    for (int i = 0; i < (int)mSoftwareList.size(); i++) {
+        int count = i+1;
+        stream << "Software" << count << "=" << mSoftwareList[i].name;
+        stream << "|" << mSoftwareList[i].size;
+        stream << "|" << mSoftwareList[i].install_time;
+        stream << "|" << mSoftwareList[i].version;
+        stream << "|" << mSoftwareList[i].product_key;
+        stream << "|" << mSoftwareList[i].product_nr;
+    }
     return stream.str();
 }
 
--- software.h
+++ software.h
@@ -9,14 +9,27 @@
 #define __SOFTWARE_H__
 
 #include <iostream>
+#include <vector>
 #include "info.h"
+#include "size.h"
 
 class Software : public Info
 {
     public:
+        struct SoftwarePackage {
+            string name;
+            string version;
+            Size size;
+            string install_time;
+            string product_key;
+            string product_nr;
+        };
         Software();
         ~Software();
         std::string output();
+
+    protected:
+        vector<SoftwarePackage> mSoftwareList;
 
 };
 
