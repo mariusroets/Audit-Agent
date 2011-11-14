3d6003352089539f3e24324762aecf0368ca3cf9
Marius Roets <roets.marius@gmail.com>
2011-11-14 11:48:13 +0200

    Bug fix: colon missing in key Bug fix: "Volumes" missing from key

--- macharddrive.cpp
+++ macharddrive.cpp
@@ -18,7 +18,7 @@ void MacHardDrive::read()
     vector<string> device_list = SYS->children("Serial-ATA:Serial-ATA Bus");
     for (int i = 0; i < (int)device_list.size(); i++) {
         // Get the physical drives
-        string key_base = string("Serial-ATA:Serial-ATA Bus") + device_list[i] + ":"; 
+        string key_base = string("Serial-ATA:Serial-ATA Bus:") + device_list[i] + ":"; 
         mDevices.push_back(DiskDevice());
         string key = key_base + "Model";
         mDevices[i].Name = SYS->value(key);
@@ -33,16 +33,16 @@ void MacHardDrive::read()
         key = key_base + "Volumes";
         vector<string> partition_list = SYS->children(key); 
         for (int j = 0; j < (int)partition_list.size(); j++) {
+            string key_base2 = key_base + "Volumes:" + partition_list[j] + ":";
             mDevices[i].Partitions.push_back(Partition());
             mDevices[i].Partitions[j].Name = partition_list[j];
-            key_base = key_base + partition_list[j] + ":";
-            key = key_base + "Capacity";
+            key = key_base2 + "Capacity";
             mDevices[i].Partitions[j].Capacity = Size(SYS->value(key));
-            key = key_base + "Available";
+            key = key_base2 + "Available";
             mDevices[i].Partitions[j].Avail = Size(SYS->value(key));
-            key = key_base + "File System";
+            key = key_base2 + "File System";
             mDevices[i].Partitions[j].FileSystem = SYS->value(key);
-            key = key_base + "Mount Point";
+            key = key_base2 + "Mount Point";
             mDevices[i].Partitions[j].MountPoint = SYS->value(key);
             mDevices[i].Partitions[j].Mounted = true;
         }
