9ef6105336a5cb8d3be9d5b9493addeea5764ae9
Marius Roets <marius.roets@eskom.co.za>
2012-08-07 16:59:02 +0200

    Get previously generated unique id so that it is reported in output file Added scancomp constant

--- linuxasset.cpp
+++ linuxasset.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include "commandparser.h"
 #include "linuxasset.h"
+#include "util.h"
 
 LinuxAsset::LinuxAsset()
 {
@@ -14,7 +15,9 @@ void LinuxAsset::read()
     CommandParser parser;
     std::vector<std::string> lines = parser.parse("hostname");
     std::vector<std::vector<std::string> > fields = parser.split(".");
+    mScanComp = "4.1.0.13";
     mMachineName = fields[0][0];
+    mUniqueId = Util::SETTINGS->unique_id;
     lines = parser.parse("whoami");
     mUser = lines[0];
 }
