17ac2ee83c0d33182b5c1622c179f77d045b3855
Marius Roets <marius.roets@eskom.co.za>
2012-08-23 17:06:10 +0200

    Added status link Removed log file output

--- deploy/agent_service.sh
+++ deploy/agent_service.sh
@@ -5,7 +5,7 @@ start()
 {
     echo "Starting Lattitude Audit Agent "
     echo " log location is:" $logdir
-    "$prefix/$installdir/agent" -d start >> "$logdir/$servicelog"
+    "$prefix/$installdir/agent" -d start
     echo "Done starting Lattitude Audit Agent! "
 
 }
@@ -13,8 +13,13 @@ start()
 stop()
 {
     echo "Stopping Lattitude Audit Agent "
-    "$prefix/$installdir/agent" -d stop >> "$logdir/$servicelog"
+    "$prefix/$installdir/agent" -d stop
     echo "Done stopping Lattitude Audit Agent! "
+}
+status()
+{
+    echo "Determining status of Lattitude Audit Agent "
+    "$prefix/$installdir/agent" -d status
 }
 
 # See how we were called.
@@ -26,7 +31,7 @@ case "$1" in
         stop
     ;;
     status)
-        status -p $pidfile $processname
+        status
         RETVAL=$?
     ;;
     restart)
