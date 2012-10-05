4ac4968092d0c6f265df5748141fbface3e6d37f
Marius Roets <roetsm@marius-laptop-12-2.eksom.co.za>
2012-10-05 10:25:50 +0200

    Linux software list based on rpm database implemented

--- linuxsoftware.cpp
+++ linuxsoftware.cpp
@@ -1,5 +1,7 @@
 
 #include "linuxsoftware.h"
+#include "commandparser.h"
+#include "util.h"
 
 LinuxSoftware::LinuxSoftware()
 {
@@ -9,4 +11,32 @@ LinuxSoftware::~LinuxSoftware()
 }
 void LinuxSoftware::read()
 {
+    CommandParser cmdparser;
+    cmdparser.parse("rpm", "-qai", true);
+    std::vector<std::string> lines = cmdparser.lines();
+    cmdparser.split(":");
+    std::vector<std::vector<std::string> > fields = cmdparser.fields();
+
+    int count = -1;
+    for (int i = 0; i < (int)fields.size(); i++) {
+        if (fields[i].size() <= 1)
+            continue;
+        if (fields[i][0] == "Name") {
+            count++;
+            mSoftwareList.push_back(SoftwarePackage());
+            mSoftwareList[count].name = fields[i][1];
+        }
+        if (fields[i][0] == "Version") {
+            mSoftwareList[count].version = fields[i][1];
+        }
+        if (fields[i][0] == "Install Date") {
+            mSoftwareList[count].install_time = fields[i][1];
+        }
+        if (fields[i][0] == "Size") {
+            mSoftwareList[count].size = Size(fields[i][1]+"B");
+            mSoftwareList[count].size.convertTo(Size::KB);
+
+        }
+
+    }
 }
