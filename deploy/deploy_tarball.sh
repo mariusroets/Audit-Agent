10cd4f2307f073cd4162da27f66d6bec33a9e135
Marius Roets <roets.marius@gmail.com>
2017-11-27 13:28:27 +0200

    Update README and deploy script

--- deploy/deploy_tarball.sh
+++ deploy/deploy_tarball.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 . constants.sh
-
-scp $tarball root@"$1":$prefix/
-ssh root@"$1" "cd $prefix; tar -xzvf $tarball; cd '$installdir'; ./deploy_init.sh" 
+if [ -z "$2" ]; then
+    echo Doing local deploy...
+    cp $tarball $prefix/
+    cd $prefix
+    tar -xzvf $tarball
+    cd "$installdir"
+    ./deploy_init.sh
+else
+    echo Doing remote deploy deploy to $2...
+    scp $tarball root@"$1":$prefix/
+    ssh root@"$1" "cd $prefix; tar -xzvf $tarball; cd \"$installdir\"; ./deploy_init.sh" 
+fi
